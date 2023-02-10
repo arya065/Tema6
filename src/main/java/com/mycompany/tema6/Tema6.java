c9e69c3e32f6724ceef77f88dc9c2a5a53674378
arya <arya065@g.educaand.es>
2023-02-10 10:41:42 +0100

    classes

---
+++ src/main/java/com/mycompany/tema6/Tema6.java
@@ -0,0 +1,9 @@
+package com.mycompany.tema6;
+
+
+public class Tema6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/ej1/test.java
@@ -0,0 +1,11 @@
+package ej1;
+
+import java.util.*;
+import java.time.*;
+
+public class test {
+
+    public static void main(String[] args) {
+
+    }
+}
